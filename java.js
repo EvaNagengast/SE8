40cd4680dcc66cdc2e6179d4e725746bbd793ed9
SpiceWeasel66 <nagengasteva@gmail.com>
2023-08-21 10:45:14 +0100

    Added Songs from Spotify

---
+++ java.js
@@ -0,0 +1,18 @@
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+  const element = document.getElementById("songTwo");
+  const options = {
+    width: `40%`,
+    height: `300`,
+    uri: "spotify:track:5jzma6gCzYtKB1DbEwFZKH",
+  };
+
+  const callback = (EmbedController) => {
+    document.querySelectorAll(".episode").forEach((episode) => {
+      episode.addEventListener("click", () => {
+        EmbedController.loadUri(episode.dataset.spotifyId);
+      });
+    });
+  };
+
+  IFrameAPI.createController(element, options, callback);
+};
